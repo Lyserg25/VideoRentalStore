2af2d2d6938bfc1fc69537c6bc55d7254eba6663
Unknown <s0539720@htw-berlin.de>
2017-07-30 12:01:53 +0200

    fix fixtures and products controller tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { amount: @product.amount, description: @product.description, title: @product.title } }
+      post products_url, params: { product: { category_id: @product.category.id, amount: @product.amount, genre_id: @product.genre.id, description: @product.description, title: @product.title } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +34,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { amount: @product.amount, description: @product.description, title: @product.title } }
+    patch product_url(@product), params: { product: { category_id: @product.category.id, amount: @product.amount, genre_id: @product.genre.id, description: @product.description, title: @product.title } }
     assert_redirected_to product_url(@product)
   end
 
