fea626d4b427f3a4530237e18163de6d5545daad
Unknown <s0539720@htw-berlin.de>
2017-07-31 07:20:21 +0200

    user db seeds and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705161252) do
+ActiveRecord::Schema.define(version: 20170731050318) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20170705161252) do
     t.integer "genre_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["genre_id"], name: "index_products_on_genre_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+User.create(name: "Admin", password: "pw_admin", password_confirmation: "pw_admin")
+
+Category.delete_all
 c1 = Category.create(name: "DVD")
 c2 = Category.create(name: "Blu-Ray")
 c3 = Category.create(name: "PS4 Game")
 c4 = Category.create(name: "PC Game")
-#c.products.create(title: "Harry Potter", description:"Harry Potter", amount: "2")
 
+
+Genre.delete_all
 g1 = Genre.create(name: "Drama")
 g2 = Genre.create(name: "Thriller")
 g3 = Genre.create(name: "Comedy")
 g4 = Genre.create(name: "Horror")
 
+Product.delete_all
 Product.create(title: "Ghostwriter", description:"Description", genre: g1, amount: "2", category: c1)
 Product.create(title: "Disturbia", description:"Description", genre: g2, amount: "2", category: c2)
 Product.create(title: "The Ring", description:"Description", genre: g1, amount: "2", category: c1)
