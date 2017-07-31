d66e8599b079892f4bf0b2f1fef12e78d7aa5555
Unknown <s0539720@htw-berlin.de>
2017-07-31 14:27:17 +0200

    created comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :comments
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
